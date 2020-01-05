0f7de97d269cbbc981b979a172c77be770e1f70f
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-01-05 01:13:26 +0000

    added stuff

--- main.c
+++ main.c
@@ -1,1 +1,30 @@
-#include <monty.h>
+#include "monty.h"
+/**
+  *
+  *
+  */
+int main(int ac, char *argv[])
+{
+	FILE *fp;
+	int i;
+	stack_t *top = NULL;
+	if (ac == 2)
+	{
+		fp = fopen(argv[1], "r");
+		if (fp == NULL)
+		{
+			fprintf(stderr ,"Error: Can't open file %s\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+		//still doing the getline or fgets part
+		for (i = 1; !feof; i++)
+		{
+		}
+		fclose(fp);
+	}
+	else
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+}
---
+++ stack_functions.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+  *
+  *
+  */
+void push(int x)
+{
+	stack_t *newNode;
+
+	newNode = top;
+	if (newNode == NULL)
+	{
+		newNode == malloc(sizeof(stack_t));
+		newNode->n = x;
+		newNode->next = NULL;
+		newNode ->prev =;
+	}
+	else
+	{
+	}
+}
